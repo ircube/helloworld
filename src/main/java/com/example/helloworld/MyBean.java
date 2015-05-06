be38cba595883a4c09a8908323dd91f56350fd56
Ivo <ivo753@gmail.com>
2015-05-06 18:28:01 -0400

    deleted target
    target folder has been deleted

---
+++ src/main/java/com/example/helloworld/MyBean.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+/**
+ * Created by conedinka02 on 06/05/2015.
+ */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
